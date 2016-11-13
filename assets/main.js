cdbf333f41d27df76c0cc03d5e8f7ecfae0fd109
Carlos Troyano Carmona <carlos.troyano.carmona@gmail.com>
2016-11-13 18:55:34 +0000

    cambiado nombre assets

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,16 @@
-
+/**********************************************************
+ * JSBox 
+ * JSBox is an open source library that contains a series  
+ * of graphic modules created for your web page.
+ * It is based on JQuery so you have to install it to use 
+ * it.
+ * 
+ * 
+ * 
+ * Athor: Carlos Troyano Carmona
+ * 
+ * *********************************************************
+ * */
 window.jSBoxed = (function () {
     function JSBoxed (els) {
     }
@@ -56,23 +68,8 @@ window.jSBoxed = (function () {
                 }
                  
                 return frame;
-             }
-             this.context.append(clk());
-             
-            function getRotationDegrees(obj) {
-                var matrix = obj.css("-webkit-transform") ||
-                obj.css("-moz-transform")    ||
-                obj.css("-ms-transform")     ||
-                obj.css("-o-transform")      ||
-                obj.css("transform");
-                if(matrix !== 'none') {
-                    var values = matrix.split('(')[1].split(')')[0].split(',');
-                    var a = values[0];
-                    var b = values[1];
-                    var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-                } else { var angle = 0; }
-                return (angle < 0) ? angle + 360 : angle;
             }
+            this.context.append(clk());
             
             function rotateHourSticks() {
                 var degrees = 0;
